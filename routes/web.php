434ec6885f4f1d3ebb9d5042bc5a1088f5c2ca79
SU1CatDEV <gergenkopzoya@gmail.com>
2025-02-01 20:34:40 +0700

    sem 3 - added employees migration, model, controller and seeder. added test_database route.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 use \App\Http\Controllers\FormProcessor;
+use \App\Models\Employee;
 
 Route::get('/userform', [FormProcessor::class, 'index'])->name('userform');
 Route::post('/store_form', [FormProcessor::class, 'store'])->name('store_form');
 Route::get('/greet', [FormProcessor::class, 'greet'])->name('greet');
+
+Route::get('/test_database', function () {
+    $employee1 = new Employee;
+    $employee1->name = "John Johnson";
+    $employee1->age = 25;
+    $employee1->salary = 100000;
+    $employee1->role = "Programmer";
+    $employee1->save();
+});
